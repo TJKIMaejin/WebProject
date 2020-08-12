4fd67f3ec80a11e1091aedf7cc06c78168e847be
develop-sell <brianpark.3@hanmail.net>
2020-08-12 13:55:07 +0900

    fix : index.html template 폴더로 이동, app.py local 돌리는 것 확인

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
-import http.server
-import socketserver
+from flask import Flask, render_template
+app = Flask(__name__)
 
-handler = http.server.SimpleHTTPRequestHandler
+@app.route('/')
+def home():
+  return render_template('index.html')
 
-with socketserver.TCPServer(('', 8080), handler) as httpd:
-  print('Server listening on port 8080...')
-  httpd.serve_forever()
+if __name__ == '__main__':
+  app.run('0.0.0.0', port=5000, debug = True)
+
