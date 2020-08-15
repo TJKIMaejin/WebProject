43483fc4609117b8addff4f02a436766879c0763
TJ <53690207+TJKIMaejin@users.noreply.github.com>
2020-08-15 22:11:56 +0900

    분류 추가

--- crawling2.py
+++ crawling2.py
@@ -8,6 +8,7 @@ db = client.recipe3
 headers ={"User-agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.80 Safari/537.36"}
 
 i=range(1,11)
+page=[]
 recipe=[]
 for a in i:
 
@@ -15,10 +16,12 @@ for a in i:
         data = requests.get('https://www.10000recipe.com/bbs/list.html?bid=3&cid=2&page=' + str(a), headers=headers)
         soup = BeautifulSoup(data.text, 'html.parser')
         num = soup.find('div', {'class': 'info_list'})
+        k=num.find_all('div',{'class':'media'})
 
-        k=num.find_all('h4',{'class':'media-heading'})
+
         for kk in k:
-            (recipe.append(kk.find('a').text))
+            page.append(kk.find('a')['href'])
+            # (recipe.append(kk.find('a').text))
 
 
 
@@ -26,12 +29,35 @@ for a in i:
 
     except:
         pass
+name=[]
+category=[]
+# print(page)
 
-print(recipe)
-k = range(0, len(recipe))
+for p in page:
+    data2 = requests.get('https://www.10000recipe.com'+p,headers=headers)
+    soup = BeautifulSoup(data2.text, 'html.parser')
+    try:
+        name1 =soup.select('#contents_area')
+        cate =soup.find("dl",{"class":"recipe_cont"}).find_all("td")[2]
 
+        category.append(cate.text)
+
+        for craw in name1:
+            a_tag =craw.select_one('#contents_area > div>h2')
+
+
+
+        name.append(a_tag.text)
+
+
+
+    except:
+        pass
+
+k= range(0,len(page))
 for i in k:
     doc = {
-        'recipe': recipe[i]
+        'name': name[i],
+        'category':category[i]
     }
     db.recipe3.insert_one(doc)
