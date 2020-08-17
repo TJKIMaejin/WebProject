dd079f196d6a946c13a72c423cd29be4fb67919b
TJ <53690207+TJKIMaejin@users.noreply.github.com>
2020-08-17 18:17:33 +0900

    건들 ㄴ

--- tmp.py
+++ tmp.py
@@ -1,32 +1,27 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request ,jsonify
 import threading
 app = Flask(__name__)
 
+from pymongo import MongoClient           # pymongo를 임포트 하기(패키지 인스톨 먼저 해야겠죠?)
+client = MongoClient('localhost', 27017)  # mongoDB는 27017 포트로 돌아갑니다.
+db = client.recipe
 @app.route('/') # 페이지 연결: 루트페이지
-
 
 def home():
     """
     index.html 페이지 연결.
-    """
+    """               
     return render_template('index.html')
 
-@app.route('/test', methods=['POST'])
-def test_post():
-    """ 
-    recipe.html 페이지에서 타이틀(title) 값 요청.
+@app.route('/main',methods=['GET']) # 페이지 연결: 루트페이지
+
+def yun():
     """
-    title_receive = request.form['title']
-    print(title_receive)
-    return jsonify({'result':'success', 'msg': '이 요청은 POST!'})
-
-
-@app.route('/test', methods=['GET'])
-def test_get():
-    title_receive = request.args.get('title')
-    print(title_receive)
-    return jsonify({'result':'success', 'msg': '이 요청은 GET!'})
-
+    index.html 페이지 연결.
+    """               
+    result = list(db.recipe.find({},{'_id':0}))
+    return render_template('index.html')
+    eturn jsonify({'result':'success', 'result':result})
 
 if __name__ == '__main__':
   app.run('0.0.0.0', port=5000, debug = True)
