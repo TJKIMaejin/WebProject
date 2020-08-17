2b631df09e74c88f75a4e2ca20c7380c90cf2deb
yuna1212 <cya3559@naver.com>
2020-08-17 17:03:46 +0900

    윤아 tmp 수정

--- tmp.py
+++ tmp.py
@@ -1,9 +1,34 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import threading
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/') # 페이지 연결: 루트페이지
+
+
 def home():
-  return render_template('index.html')
+    """
+    index.html 페이지 연결.
+    """
+    return render_template('index.html')
+
+@app.route('/test', methods=['POST'])
+def test_post():
+    """ 
+    recipe.html 페이지에서 타이틀(title) 값 요청.
+    """
+    title_receive = request.form['title']
+    print(title_receive)
+    return jsonify({'result':'success', 'msg': '이 요청은 POST!'})
+
+
+@app.route('/test', methods=['GET'])
+def test_get():
+    title_receive = request.args.get('title')
+    print(title_receive)
+    return jsonify({'result':'success', 'msg': '이 요청은 GET!'})
+
 
 if __name__ == '__main__':
-  app.run('0.0.0.0', port=5000, debug = True) 
+  app.run('0.0.0.0', port=5000, debug = True)
+  #test_post()
+  print("끝!")
