655ae99fcd28a3f22af26d06035ed00f393f77b9
GOHEEA <gmldk728@naver.com>
2020-08-26 15:55:28 +0900

    db 연결

--- static/js/index.js
+++ static/js/index.js
@@ -30,12 +30,13 @@ function listing() {
 }
 
 
+
+
 function catListing(Val) {
 
   let temp_html = ''
   let gus = []
   
-
   if (Val == '알류') {
     gus = ["계란", "달걀", "메추리알", "날치알", "거위알"]
 
@@ -48,22 +49,25 @@ function catListing(Val) {
     $('#b1').empty()
     $('#b1').append(temp_html)
     
-
-  } else if (Val == '유제품') {
-    gus = ["우유", "연유", "버터", "치즈", "모짜렐라치즈", "바나나우유", "바나나아이스크림", "분유", "스트링치즈", "요거트", "연유", "요구르트", "우유", "체다치즈", "크림치즈",
+  } if (Val == '유제품') {
+    gus = ["우유", "연유", "버터", "치즈", "모짜렐라치즈", "바나나우유", "바나나아이스크림", "분유", "스트링치즈", "요거트", "요구르트", "우유", "체다치즈", "크림치즈",
       "탈지분유", "파마산치즈", "파마산치즈가루"]
 
+    
     for (let i = 0; i < gus.length; i++) {
+      if(i%10==0){
+        temp_html+='<div></div>'
+      }
       temp_html +=
         '<input type="checkbox" name="' + Val + '" value="' + i + 1 + '">' + gus[i] + ''
-  
+         
     }
   
     $('#b2').empty()
     $('#b2').append(temp_html)
     
     
-  } else if (Val == '유지류') {
+  } if (Val == '유지류') {
     gus = ["코코넛오일", "콩기름", "튀김기름"]
     for (let i = 0; i < gus.length; i++) {
       temp_html +=
@@ -73,7 +77,7 @@ function catListing(Val) {
   
     $('#b3').empty()
     $('#b3').append(temp_html)
-  } else if (Val == '견과류') {
+  } if (Val == '견과류') {
     gus = ["아몬드", "아몬드가루", "잣", "캐슈넛", "피칸", "호두"]
     for (let i = 0; i < gus.length; i++) {
       temp_html +=
@@ -83,13 +87,16 @@ function catListing(Val) {
   
     $('#b4').empty()
     $('#b4').append(temp_html)
-  } else if (Val == '과일류') {
+  } if (Val == '과일류') {
     gus = ["과일", "건크랜베리", "크랜베리", "건포도", "포도", "귤", "딸기", "냉동딸기", "딸기잼", "냉동블루베리", "블루베리", "레몬",
       "레몬제스트", "레몬즙", "매실", "바나나", "배", "배즙", "복숭아", "사과", "사과즙", "아보카도", "오렌지", "체리", "크랜베리", "키위", "통조림파인애플",
       "파인애플"
     ]
 
     for (let i = 0; i < gus.length; i++) {
+      if(i%10==0){
+        temp_html+='<div></div>'
+      }
       temp_html +=
         '<input type="checkbox" name="' + Val + '" value="' + i + 1 + '">' + gus[i] + ''
   
@@ -97,7 +104,7 @@ function catListing(Val) {
   
     $('#b5').empty()
     $('#b5').append(temp_html)
-  } else if (Val == '육류') {
+  } if (Val == '육류') {
     gus = ["고기", "차돌박이", "닭", "닭가슴살", "닭날개", "닭다리", "닭똥집", "닭봉", "닭안심", "대패삼겹살", "돈가스",
       "돼지갈비", "돼지고기", "돼지고기간것", "돼지고기불고기용", "돼지고기잡채용", "돼지고기찌개용", "돼지등뼈", "돼지등심", "돼지목살",
       "돼지안심", "돼지앞다리살", "베이컨", "부채살", "비엔나소시지", "삼겹살", "소갈비", "소고기", "소고기간것", "소고기국거리", "소고기다진것", "소고기등심",
@@ -106,6 +113,9 @@ function catListing(Val) {
     ]
 
     for (let i = 0; i < gus.length; i++) {
+      if(i%8==0){
+        temp_html+='<div></div>'
+      }
       temp_html +=
         '<input type="checkbox" name="' + Val + '" value="' + i + 1 + '">' + gus[i] + ''
   
@@ -113,10 +123,13 @@ function catListing(Val) {
   
     $('#b6').empty()
     $('#b6').append(temp_html)
-  } else if (Val == '콩류') {
+  } if (Val == '콩류') {
     gus = ["검은콩", "견과류", "두부", "두유", "땅콩", "땅콩가루", "땅콩버터", "볶은땅콩", "연두부", "완두콩", "콩가루", "팥"]
 
     for (let i = 0; i < gus.length; i++) {
+      if(i%10==0){
+        temp_html+='<div></div>'
+      }
       temp_html +=
         '<input type="checkbox" name="' + Val + '" value="' + i + 1 + '">' + gus[i] + ''
   
@@ -124,12 +137,15 @@ function catListing(Val) {
   
     $('#b7').empty()
     $('#b7').append(temp_html)
-  } else if (Val == '곡류') {
+  } if (Val == '곡류') {
     gus = ["박력쌀가루", "쌀", "쌀가루", "오트밀", "옥수수", "옥수수가루", "옥수수통조림",
       "찹쌀", "찹쌀가루", "캔옥수수", "통밀가루"
     ]
 
     for (let i = 0; i < gus.length; i++) {
+      if(i%10==0){
+        temp_html+='<div></div>'
+      }
       temp_html +=
         '<input type="checkbox" name="' + Val + '" value="' + i + 1 + '">' + gus[i] + ''
   
@@ -137,10 +153,13 @@ function catListing(Val) {
   
     $('#b8').empty()
     $('#b8').append(temp_html)
-  } else if (Val == '해조류') {
+  } if (Val == '해조류') {
     gus = ["다시마", "건다시마", "다시마육수", "김", "김가루", "김밥김", "김자반", "메생이", "미역", "조미김", "톳", "파래"]
 
     for (let i = 0; i < gus.length; i++) {
+      if((i+1)%12==0){
+        temp_html+='<div></div>'
+      }
       temp_html +=
         '<input type="checkbox" name="' + Val + '" value="' + i + 1 + '">' + gus[i] + ''
   
@@ -148,12 +167,15 @@ function catListing(Val) {
   
     $('#b9').empty()
     $('#b9').append(temp_html)
-  } else if (Val == '어패류') {
+  } if (Val == '어패류') {
     gus = ["가자미", "갈치", "고등어", "고등어통조림", "참치", "참치캔", "고추참치", "꽁치", "꽁치통조림", "낙지", "문어", "동태", "멸치",
       "명란", "명란젓", "모시조개", "삼치", "바지락", "연어", "연어캔", "잔멸치", "장어", "조기", "중멸치", "지리멸치",
       "참치통조림", "코다리", "홍합", "황태", "황태채", "황태포", "훈제연어"
     ]
     for (let i = 0; i < gus.length; i++) {
+      if(i%10==0){
+        temp_html+='<div></div>'
+      }
       temp_html +=
         '<input type="checkbox" name="' + Val + '" value="' + i + 1 + '">' + gus[i] + ''
   
@@ -161,11 +183,14 @@ function catListing(Val) {
   
     $('#b10').empty()
     $('#b10').append(temp_html)
-  } else if (Val == '갑각류') {
+  } if (Val == '갑각류') {
     gus = ["냉동새우", "보리새우", "건새우", "새우", "대하", "오징어", "갑오징어", "물오징어", "게살", "관자", "꼬막", "꽃게",
       "자숙새우", "칵테일새우", "크래미", "흰다리새우"
     ]
     for (let i = 0; i < gus.length; i++) {
+      if(i%10==0){
+        temp_html+='<div></div>'
+      }
       temp_html +=
         '<input type="checkbox" name="' + Val + '" value="' + i + 1 + '">' + gus[i] + ''
   
@@ -173,11 +198,14 @@ function catListing(Val) {
   
     $('#b11').empty()
     $('#b11').append(temp_html)
-  } else if (Val == '버섯류') {
+  } if (Val == '버섯류') {
     gus = ["팽이버섯", "목이버섯", "만가닥버섯", "맛타리버섯", "건표고버섯", "표고버섯", "백만송이버섯", "버섯", "새송이버섯",
       "애느타리버섯", "양송이버섯", "팽이버섯", "표고버섯", "표고버섯가루"
     ]
     for (let i = 0; i < gus.length; i++) {
+      if(i%9==0){
+        temp_html+='<div></div>'
+      }
       temp_html +=
         '<input type="checkbox" name="' + Val + '" value="' + i + 1 + '">' + gus[i] + ''
   
@@ -185,7 +213,7 @@ function catListing(Val) {
   
     $('#b12').empty()
     $('#b12').append(temp_html)
-  } else if (Val == '채소류') {
+  } if (Val == '채소류') {
     gus = ["깻잎", "숙주나물", "다진마늘", "다진생강", "청양고추", "콩나물", "비름나물", "대파", "양파", "고수", "가지", "감자", "감자전분", "건고추", "고추", "청양고추", "고구마", "고구마줄기", "김치", "냉이", "꽈리고추", "브로콜리",
       "느타리버섯", "단무지", "단호박", "달래", "당근", "대추", "대파", "더덕", "도라지", "도토리묵", "돌나물", "돌미나리", "돼지호박",
       "두릅", "로메인", "로즈마리", "마늘", "마늘종", "무", "무말랭이", "무순", "미나리", "밤", "방울양배추", "방울토마토", "방풍나물",
@@ -198,6 +226,9 @@ function catListing(Val) {
       "케일", "콩나물", "토마토", "파", "파프리카", "피망", "호박"
     ]
     for (let i = 0; i < gus.length; i++) {
+      if(i%10==0){
+        temp_html+='<div></div>'
+      }
       temp_html +=
         '<input type="checkbox" name="' + Val + '" value="' + i + 1 + '">' + gus[i] + ''
   
@@ -217,6 +248,9 @@ function catListing(Val) {
       "페퍼론치노", "풋고추", "피시소스", "허니머스터드", "홀그레인머스터드",
     ]
     for (let i = 0; i < gus.length; i++) {
+      if(i%10==0){
+        temp_html+='<div></div>'
+      }
       temp_html +=
         '<input type="checkbox" name="' + Val + '" value="' + i + 1 + '">' + gus[i] + ''
   
@@ -237,6 +271,9 @@ function catListing(Val) {
       "호박잎", "화이트와인", "화이트초콜릿", "후리가케", "흑임자"
     ]
     for (let i = 0; i < gus.length; i++) {
+      if(i%10==0){
+        temp_html+='<div></div>'
+      }
       temp_html +=
         '<input type="checkbox" name="' + Val + '" value="' + i + 1 + '">' + gus[i] + ''
   
@@ -245,10 +282,5 @@ function catListing(Val) {
     $('#b15').empty()
     $('#b15').append(temp_html)
   }
-
-  
-
-
-
 
 }
