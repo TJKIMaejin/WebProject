2577d2056c1e1bae85d4aa7b8288962307d7c230
GOHEEA <gmldk728@naver.com>
2020-09-02 17:15:06 +0900

    밀려나는 오류 수정 완료

--- static/js/index.js
+++ static/js/index.js
@@ -66,10 +66,10 @@ function seil(element, orders)
             console.log(material[k]);
             recipe_final.push(orders[0][j]["title"]);
 
-            picture_html += '<div class = "c'+(count%3+1)+' ">\
+            picture_html += '<div class = "c1 ">\
             <div class="c1-project-img"  id="food" style="background-image: url(' +orders[0][j]["img"]+ ')"></div>\
             <div class = "c1-project-explain">\
-            <p>"'+orders[0][j]["title"]+'"</p>\
+            <p class="c1-project-t">"'+orders[0][j]["title"]+'"</p>\
             <div class = "c1-project-btn">\
             <button type="button" onclick="location.href= \'http://localhost:5000/\' ">선택하기</button>\
             </div>\
